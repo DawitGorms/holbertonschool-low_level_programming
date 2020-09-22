298bc16623864116ed2177604396af5d6f38bc33
huy75 <huy.nguyen66@gmail.com>
2020-09-22 19:49:50 +0000

    Change 102 to non recursive

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 
 /**
- * fibonacci - recursive function
- * @n: fibonacci n
- * Return: result
- */
-
-long fibonacci(long n)
-{
-	if (n == 0 || n == 1)
-		return (n);
-	return (fibonacci(n - 1) + fibonacci(n - 2));
-}
-
-/**
  * main - prints the first 50 Fibonacci numbers
  * starting with 1 and 2, followed by a new line.
  *
@@ -22,10 +9,20 @@ long fibonacci(long n)
 
 int main(void)
 {
-	long ct;
+	unsigned long ct, f2, f1;
+	int i = 0;
 
-	for (ct = 2; ct < 50; ct++)
-		printf("%ld, ", fibonacci(ct));
-	printf("%ld\n", fibonacci(ct));
+	f2 = 1;
+	f1 = 2;
+	ct = f2 + f1;
+	printf("%lu, %lu, ", f2, f1);
+	for (i = 4; i < 51; i++)
+	{
+		printf("%lu, ", ct);
+		f2 = f1;
+		f1 = ct;
+		ct = f2 + f1;
+	}
+	printf("%lu\n", ct);
 	return (0);
 }
