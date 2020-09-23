db48244f453cd63c2d30e07f066085e5f8561737
huy75 <huy.nguyen66@gmail.com>
2020-09-23 17:33:58 +0000

    first try 104-fibonacci up to 90

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define N 1000000000
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	unsigned long fn, fn2, fn1, fbl, fbr, f1l, f1r, f2l, f2r;
+	int seq;
+
+	fn2 = 1;
+	fn1 = 2;
+	fn = fn2 + fn1;
+	seq = 0;
+
+	printf("%lu, %lu, ", fn2, fn1);
+
+	for (seq = 4; seq < 47; seq++)
+	{
+		printf("%09lu, ", fn);
+		fn2 = fn1;
+		fn1 = fn;
+		fn = fn2 + fn1;
+	}
+
+	f1l = fn1 / N;
+	f1r = fn1 % N;
+	fbl = fn / N;
+	fbr = fn % N;
+
+	for (seq = 47; seq < 90; seq++)
+	{
+		printf("%lu%09lu, ", fbl, fbr);
+		f2l = f1l;
+		f2r = f1r;
+		f1l = fbl;
+		f1r = fbr;
+
+		fbl = (f2l + f1l) + ((f2r + f1r) / N);
+		fbr = (f2r + f1r) % N;
+	}
+	printf("%lu%09lu\n", fbl, fbr);
+
+	return (0);
+}
