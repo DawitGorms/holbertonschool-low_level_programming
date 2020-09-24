574673e1dbb9d8b3a49f544a4260b888a0596fc8
huy75 <huy.nguyen66@gmail.com>
2020-09-24 19:22:19 +0000

    New improved 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,24 +8,12 @@
  */
 void print_number(int n)
 {
-	int index, remain;
-	char num[5];
+	int m;
 
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		remain = (n < 0) ? -n : n;
-		for (index = 0; remain > 0; index++)
-		{
-			num[index] = (remain % 10) + 48;
-			remain /= 10;
-		}
-		num[index] = 0;
-
-		if (n < 0)
-			_putchar('-');
-		for (; index >= 0; index--)
-			_putchar(num[index]);
-	}
+	m = (n < 0) ? -n : n;
+	if (n < 0)
+		_putchar('-');
+	if (m / 10)
+		print_number(m / 10);
+	_putchar(m % 10 + 48);
 }
