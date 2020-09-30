76a13377e739e3c5fc8672ce238f3aa5b165bc8f
huy75 <huy.nguyen66@gmail.com>
2020-09-30 11:31:43 +0000

    fix modify if condition

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,6 @@
 
 /**
  * print_array - prints n elements of an array of integers
- *
  * @a: the input array.
  * @n: the number of elements to be printed.
  *
@@ -17,7 +16,7 @@ void print_array(int *a, int n)
        	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i < (n - 1))
+		if (i != (n - 1))
 		    printf(", ");
 
 	}
