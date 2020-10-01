5f5b9808b55ced8f913d90b5fca62612636efff9
huy75 <huy.nguyen66@gmail.com>
2020-10-01 13:57:06 +0000

    fix use _strlen instead of *src in the first loop

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 /**
+ *_strlen - counts and reutns string length
+ * @s: the input string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (c = 0; *s; c++)
+		s++;
+
+	return (c);
+}
+
+
+/**
  * _strncpy - copies a string
  * @dest: the destination string
  * @src: the string that will be copied at the dest.
@@ -11,11 +28,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, sl;
 
+	sl = _strlen(src);
 	if (n > 0)
 	{
-		for (i = 0; i < n && *src; i++)
+		for (i = 0; i < n && i <= sl; i++)
 			dest[i] = src[i];
 
 		for (; i < n; i++)
