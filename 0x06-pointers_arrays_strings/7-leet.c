2fd96559d8d4e0f443f1c4a1f3c790a9bcebb241
huy75 <huy.nguyen66@gmail.com>
2020-10-01 19:42:57 +0000

    feat leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * leet: encodes a string into 1337.
+ * @s: pointer of the input string
+ *
+ * Return: char
+  */
+
+char *leet(char *s)
+{
+	char *c = s;
+	char *letters = "aAeEoOtTlL";
+	char *encode = "4433007711";
+	int i;
+
+	while (*c)
+	{
+		for (i = 0; letters[i]; i++)
+			if (*c == letters[i])
+				*c = encode[i];
+		c++;
+	}
+	return (s);
+}
