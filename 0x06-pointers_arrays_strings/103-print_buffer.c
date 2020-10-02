879dad44d0bad1eb82d1385050ef56583db8047c
huy75 <huy.nguyen66@gmail.com>
2020-10-02 21:17:59 +0000

    feat print buffer

---
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to a buffer
+ * @size: size of bytes to print
+ *
+ * Return: nothing to return.
+ */
+
+void print_buffer(char *b, int size)
+{
+
+	int c, idx, start;
+
+	if (size > 0)
+		for (start = 0; start < size; start += 10)
+		{
+			printf("%08x: ", start);
+			for (idx = 0; idx < 10; idx++)
+			{
+				printf("%02x", *(b + start + idx));
+				if (idx % 2 == 0)
+					printf(" ");
+			}
+			for (idx = 0; idx < 10; idx++)
+			{
+				c = *(b + start + idx);
+				if (c < 32 ||c == 126)
+					c = 46;
+				printf("%c", c);
+			}
+			printf("\n");
+		}
+	else
+		printf("\n");
+
+
+}
