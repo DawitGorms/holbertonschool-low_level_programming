64830a1e47057c4faa3338f51be90a7f3dfe5d72
huy75 <huy.nguyen66@gmail.com>
2020-10-05 20:01:13 +0000

    feat strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring.
+ * @haystack: the string to be scanned
+ * @needle: the string to be searched within haystack string.
+ * Return: a pointer to the first occurrence in haystack of any of the
+ * entire sequence of characters specified in needle, or a null pointer
+ * if the sequence is not present in haystack.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *start;
+	int idx =0, first, match;
+
+	for (; *haystack; haystack++)
+	{
+		match = 1;
+		if ((*haystack != *(needle + idx)))
+		{
+			match = 0;
+			first = 0;
+			idx = 0;
+			continue;
+		}
+		else
+		{
+			if (first == 0)
+			{
+				first = 1;
+				start = haystack;
+			}
+			if (*(needle + idx) == '\0')
+				break;
+			idx++;
+		}
+
+	}
+	if (match == 1)
+		return (start);
+	else
+		return (NULL);
+}
