f590edc3cfe95003cfd4a92695ab5e68c28d2156
huy75 <huy.nguyen66@gmail.com>
2020-10-07 13:34:19 +0000

    feat prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,37 @@
 #include "holberton.h"
 
 /**
+ * isSquare - helper function
+ * @n: number loooking for its sqaure
+ * @idx: iterator
+ *
+ * Return: the root of the number
+ */
+
+int isSquare(int n, int idx)
+{
+	if (idx * idx > n)
+		return (-1);
+	if (idx * idx == n)
+		return (idx);
+	return (isSquare(n, idx + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number
+ *
+ * Return: the root of the number, -1 if there is none.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (isSquare(n, 1));
+}
+
+/**
  * isPrime - helper function
  * @n: number
  * @idx: iterator
@@ -10,11 +41,11 @@
 
 int isPrime(int n, int idx)
 {
-	if ( > n)
-		return (-1);
-	if (idx * idx == n)
-		return (idx);
-	return (isSquare(n, idx + 1));
+	if (n % idx == 0)
+		return (0);
+	if (idx > _sqrt_recursion(n))
+		return (1);
+	return (isPrime(n, idx + 1));
 }
 
 /**
@@ -24,9 +55,9 @@ int isPrime(int n, int idx)
  * Return: 1 if yes, 0 if not.
  */
 
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 		return (0);
 	return (isPrime(n, 2));
 }
