f94a35f7be54e1f067acdc5b2fb9c0fcbc34f00a
huy75 <huy.nguyen66@gmail.com>
2020-10-08 14:53:17 +0000

    feat wildcard string

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -12,14 +12,18 @@
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == *s2)
+	{
 		if (*s1 == '\0')
 			return (1);
 		else
 			return (wildcmp(s1 + 1, s2 + 1));
+	}
 	if (*s2 == '*')
+	{
 		if (*s1 == '\0')
 			return (wildcmp(s1, s2 + 1));
 		else
 			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
 	return (0);
 }
