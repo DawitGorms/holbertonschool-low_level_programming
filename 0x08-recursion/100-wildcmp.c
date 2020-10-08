56475854fba002c9946ce0340db5cd97e5a0bb1b
huy75 <huy.nguyen66@gmail.com>
2020-10-08 14:47:00 +0000

    feat wildcard

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings.
+ * @s1: the pointer to the string 1
+ * @s2: can contain the special character *.
+ * The special char * can replace any string (including an empty string).
+ *
+ * Return: 1 if the strings are identical, 0 if not.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		if (*s1 == '\0')
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		if (*s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	return (0);
+}
