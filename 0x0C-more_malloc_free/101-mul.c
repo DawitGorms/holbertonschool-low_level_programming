3c8354b60ed75b8770f538e7278cd2860899ff7d
huy75 <huy.nguyen66@gmail.com>
2020-10-16 08:10:56 +0000

    fix mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,23 +17,6 @@ int _strlen(char *s)
 }
 
 /**
- * _memset - fills memory with a constant byte.
- * @s: the memory area to be filled
- * @b: the constant byte
- * @n: number of bytes to fill with char b
- * Return: a pointer to the memory area s.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *p = s;
-
-	for (; n; n--)
-		*p++ = b;
-
-	return (s);
-}
-
-/**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements
  * @size: of each element
@@ -41,6 +24,7 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int idx;
 	void *ptr;
 
 	if (size == 0 || nmemb == 0)
@@ -50,7 +34,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	_memset(ptr, 0, size * nmemb);
+	for (idx = 0; idx < nmemb * size ; idx++)
+		*((char *)ptr + idx) = 0;
 
 	return (ptr);
 }
