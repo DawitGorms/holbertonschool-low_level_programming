03daa1c2eeb4b2cffcd92489e1c99402364238b8
huy75 <huy.nguyen66@gmail.com>
2020-10-29 14:32:26 +0000

    feat free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * free_list -
- *
- * Return: 
+ * free_list - frees all nodes of a list
+ * @head: point the head of list
+ * Return: void
  */
 void free_list(list_t *head)
 {
+	list_t *next, *ptr;
 
+	if (!head)
+		return;
+
+	ptr = head;
+
+	while (ptr)
+	{
+		next = ptr-> next;
+		free(ptr->str);
+		free(ptr);
+		ptr = next;
+	}
+	head = NULL;
 }
