a9b2044ec80337d59e389dfb8bc357622bf31b50
huy75 <huy.nguyen66@gmail.com>
2020-11-02 12:20:09 +0000

    feat sum linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,11 +1,22 @@
 #include "lists.h"
 
 /**
- * sum_listint -
- *
- * Return: 
+ * sum_listint - sum
+ * @head: the head of the list
+ * Return: returns the sum of all the data (n) of the linked list.
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
+	listint_t *ptr = head;
 
+        if (!head)
+                return (0);
+
+	while (ptr)
+	{
+		sum += ptr->n;
+		ptr = ptr->next;
+	}
+	return (sum);
 }
