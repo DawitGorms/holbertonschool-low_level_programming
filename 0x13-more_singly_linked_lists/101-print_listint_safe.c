3e9b7d7c7d6d6d30a8c5fb7d7b270a2fb99ea622
huy75 <huy.nguyen66@gmail.com>
2020-11-03 16:18:18 +0000

    fix add const

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,21 +7,20 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0, dist;
-	listint_t *ptr = head;
-	listint_t *nxt;
+	size_t nodes = 0;
+	size_t dist;
+	const listint_t *ptr = head;
+	const listint_t *nxt;
 
 	while (ptr)
 	{
 		printf("[%p] %d\n", (void *)ptr, ptr->n);
 
 		nodes++;
-
 		ptr = ptr->next;
 		nxt = head;
 
 		dist = 0;
-
 		while (dist < nodes)
 		{
 			if (ptr == nxt)
@@ -29,7 +28,6 @@ size_t print_listint_safe(const listint_t *head)
 				printf("-> [%p] %d\n", (void *)ptr, ptr->n);
 				return (nodes);
 			}
-
 			nxt = nxt->next;
 			dist++;
 		}
