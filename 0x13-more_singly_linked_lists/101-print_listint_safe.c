fc74256ae9a41002fb61fb12802e2046e1e16669
huy75 <huy.nguyen66@gmail.com>
2020-11-03 12:00:03 +0000

    fix remove break & feat free safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,25 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - print all the elements of list_t
+ * print_listint_safe - print all the elements of listint_t
  * @head: the head of the list
  * Return: number of nodes
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-
 	if (!head)
 		return (98);
-	while (head)
+
+	printf("[%p] %i\n", (void *)head, head->n);
+
+	if (head > head->next)
 	{
-		printf("[%p] %i\n", (void *)head, head->n);
-		nodes++;
-		if (head > head->next)
-			head = head->next;
-		else
-		{
-			head = head->next;
-			printf("-> [%p] %i\n", (void *)head, head->n);
-			break;
-		}
+		return (1 + print_listint_safe(head->next));
 	}
-	return (nodes);
+	else
+	{
+		printf("-> [%p] %i\n", (void *)head, head->next->n);
+		return (1);
+	}
+	return (0);
 }
