2643176e0a834a9b0c78aabe17f0b5e2d6a90b19
huy75 <huy.nguyen66@gmail.com>
2020-11-03 08:37:45 +0000

    fix ensure precedence with brackets

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode = malloc(sizeof(listint_t));
 	listint_t *ptr = *head;
 
-	if (!head || !*head || !newNode)
+	if (!head || !(*head) || !newNode)
 		return (NULL);
 
 	newNode->n = n;
