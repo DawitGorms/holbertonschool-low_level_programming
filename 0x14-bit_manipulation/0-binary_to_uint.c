afa35cb1464097937f551d151b359cbf9f05e9b5
huy75 <huy.nguyen66@gmail.com>
2020-11-05 12:49:29 +0000

    fix change logical

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,10 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
-		result = (result * 2) + (*b++ - '0');
+		result <<= 1;
+		if (*b & 1)
+			result += 1;
+		b++;
 	}
 	return (result);
 }
