73689f79860c1bfb8b244961dc966f6b725788b5
huy75 <huy.nguyen66@gmail.com>
2020-11-05 14:36:02 +0000

    feat flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,20 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits -how many bits is needed to flip from one number to another
+ * @n: origin number
+ * @m: destination number
+ * Return: the number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int xor = n ^ m;
+	int count = 0;
 
+	while (xor)
+	{
+		count += xor & 1;
+		xor >>= 1;
+	}
+	return (count);
 }
