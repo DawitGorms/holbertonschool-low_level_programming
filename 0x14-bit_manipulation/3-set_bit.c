58fe25ecc2da47c2b3b0475a3a87525aac9c7b4a
huy75 <huy.nguyen66@gmail.com>
2020-11-05 13:18:47 +0000

    feat set the value of a bit to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * set_bit -
- *
- * Return: 
+ * set_bit - sets the value of a bit at index to 1
+ * @n: the number
+ * @index: the index
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index >= sizeof(n) * 8)
+		return (-1);
+	*n |= (1 << index);
+	return (1);
 }
