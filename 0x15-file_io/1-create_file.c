670ac75d01b7e8c299a8ea09acd24b1b44bad16f
huy75 <huy.nguyen66@gmail.com>
2020-11-09 12:34:03 +0000

    feat create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,41 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ *_strlen - counts and reutns string length
+ * @s: the input string
  *
- * Return: 
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (c = 0; *s; c++)
+		s++;
+
+	return (c);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to create
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
