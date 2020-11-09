51f27b319dd8ab6d623e2da5982e58477d3bf74d
huy75 <huy.nguyen66@gmail.com>
2020-11-09 13:00:34 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,8 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int bytes = 0, len = strlen(text_content);
+	int bytes, fd;
 
 	if (!filename)
 		return (-1);
@@ -18,12 +17,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	bytes = write(fd, text_content, len);
-	if (bytes == -1)
-		return (-1);
+	if (text_content)
+	{
+		bytes = write(fd, text_content, strlen(text_content));
+		if (bytes == -1)
+			return (-1);
+	}
 
 	close(fd);
 	return (1);
