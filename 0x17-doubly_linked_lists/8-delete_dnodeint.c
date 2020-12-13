a4d07ed721aa2d35beb2d2d67a09bd3a9dadbba8
huy75 <huy.nguyen66@gmail.com>
2020-12-13 16:17:27 +0000

    fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,13 +16,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	ptr = head;
-	if (!index)
-	{
-		node = *head;
-		*head = (*head)->next;
-		free(node);
-		return (1);
-	}
 
 	while (idx < index && (*ptr)->next)
 	{
