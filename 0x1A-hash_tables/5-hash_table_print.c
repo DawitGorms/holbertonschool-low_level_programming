910f46abdad4e12dcb7a292bb632b0b506dd9c85
huy75 <huy.nguyen66@gmail.com>
2021-01-14 15:17:11 +0000

    feat print / delete

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
  *
- * Return: 
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *current;
+	int first = 1;
+	unsigned long idx;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		current = ht->array[idx];
+		while (current)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			first = 0;
+			current = current->next;
+		}
+	}
+
+	printf("}\n");
 
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table to delete
  *
- * Return: 
+ * Return: void
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_node_t *current, *tmp;
+	unsigned long int idx;
 
+	if (!ht)
+		return;
+
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		current = ht->array[idx];
+		while (current)
+		{
+			tmp = current;
+			current = current->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
