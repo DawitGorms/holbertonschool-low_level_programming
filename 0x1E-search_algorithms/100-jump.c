00fe3a07bce9dafd7779cb75795ebcabe1bd7c5e
huy75 <huy.nguyen66@gmail.com>
2021-04-20 13:05:54 +0000

    fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t block, idx = 0;
+	size_t block, idx = 0, jdx = 0;
 
 	if (!array || size == 0)
 		return (-1);
@@ -27,23 +27,18 @@ int jump_search(int *array, size_t size, int value)
 		idx += block;
 	}
 
+	jdx = idx - block;
 	printf("Value found between indexes [%lu] and [%lu]\n",
-	       idx - block, idx);
+	       jdx, idx);
 
-	idx -= block;
-	while (idx < size && value > array[idx])
+	idx = idx >= size ? size - 1 : idx;
+	while (jdx <= idx)
 	{
 		printf("Value checked array[%lu] = [%d]\n",
-		       idx, array[idx]);
-		idx++;
+		       jdx, array[jdx]);
+		if (array[jdx] == value)
+			return ((int)jdx);
+		jdx++;
 	}
-	if (array[idx] == value)
-	{
-		printf("Value checked array[%lu] = [%d]\n",
-		       idx, array[idx]);
-		return ((int)idx);
-	}
-	else
-		return (-1);
 	return (-1);
 }
