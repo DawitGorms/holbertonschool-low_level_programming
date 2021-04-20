b0beddba7397b1acbf3331cc1dc0b258fad5f236
huy75 <huy.nguyen66@gmail.com>
2021-04-20 12:19:45 +0000

    fix typo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -41,7 +41,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n",
 		       idx, array[idx]);
-		return (int(idx));
+		return ((int)idx);
 	}
 	else
 		return (-1);
